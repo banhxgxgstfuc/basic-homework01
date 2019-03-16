a0b659d41c2c8e36690812427b65280e9b0adf4f
banhxgxgstfuc <48340408+banhxgxgstfuc@users.noreply.github.com>
2019-03-16 11:05:57 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<stdlib.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) 
+{
+	printf("Hello world 3a713234\n");  
+	//¦L¥X¦r¦ê¤º®e 
+	system("pause");
+	return 0;
+}
